02117b830c360b27fbf664b1275f417259f6d081
JasperSoosaar25 <jaspersoosaar9@gmail.com>
2026-05-30 20:20:07 +0300

    Create AssemblyInfo.cs

---
+++ GrannyLegacyClock/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using MelonLoader;
+
+[assembly: MelonInfo(
+    typeof(GrannyLegacyClock.ModEntry),
+    "Granny Legacy Clock",
+    "1.0.0",
+    "Jasper")]
+
+[assembly: MelonGame(null, "Granny: Legacy")]
