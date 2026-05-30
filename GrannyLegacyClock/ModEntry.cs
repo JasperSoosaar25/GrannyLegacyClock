1a95c30ea387da6bf98572e9f8fa4c31a1d39289
JasperSoosaar25 <jaspersoosaar9@gmail.com>
2026-05-30 21:15:22 +0300

    Update ModEntry.cs

--- GrannyLegacyClock/ModEntry.cs
+++ GrannyLegacyClock/ModEntry.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Reflection;
 using MelonLoader;
 using UnityEngine;
 
@@ -11,6 +13,15 @@ namespace GrannyLegacyClock
         public override void OnInitializeMelon()
         {
             MelonLogger.Msg("Granny Legacy Clock loaded.");
+
+            var resources = Assembly
+                .GetExecutingAssembly()
+                .GetManifestResourceNames();
+
+            MelonLogger.Msg("Embedded resources:");
+
+            foreach (var resource in resources)
+                MelonLogger.Msg($" - {resource}");
         }
 
         public override void OnGUI()
@@ -22,9 +33,35 @@ namespace GrannyLegacyClock
                 _style.fontSize = 30;
                 _style.alignment = TextAnchor.UpperRight;
                 _style.normal.textColor = Color.white;
+
+                try
+                {
+                    Font corsiva =
+                        Font.CreateDynamicFontFromOSFont(
+                            "Monotype Corsiva",
+                            30);
+
+                    if (corsiva != null)
+                    {
+                        _style.font = corsiva;
+                        MelonLogger.Msg(
+                            "Monotype Corsiva found on system.");
+                    }
+                    else
+                    {
+                        MelonLogger.Warning(
+                            "Monotype Corsiva not found on system.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MelonLogger.Error(
+                        $"Font error: {ex}");
+                }
             }
 
-            string time = DateTime.Now.ToString("HH:mm:ss");
+            string time =
+                DateTime.Now.ToString("HH:mm:ss");
 
             GUI.Label(
                 new Rect(
